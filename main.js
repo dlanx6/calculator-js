42fce73c1f8890df9f88ff7d2c715c2fc68d588f
dlanx6 <144339211+dlanx6@users.noreply.github.com>
2024-11-15 23:01:11 +0800

    done

--- main.js
+++ main.js
@@ -1,38 +1,34 @@
-const calculator = document.getElementById('calculator-container');
+const buttons = document.getElementById('buttons');
 const display = document.getElementById('result');
 
 // Storing of mathematical expression
 let expression = "";
 
 // Button click event
-calculator.addEventListener('click', event => {
+buttons.addEventListener('click', event => {
     // Button click value
     const key = event.target.value;
 
     // Calculate expression if equal sign is pressed
-    if (key === "=") 
+    if (key === "=" && expression) 
     {
         // replace symbols with actual arithmetic operators in JavaScript
-        expression = expression.replace("÷", "/").replace("x", "*");
+        expression = expression.replaceAll("÷", "/").replaceAll("x", "*");
+        
+        // Calculate string as a mathematical expression
+        const result = Function("return " + expression)();
 
-        try {
-            // Alternative for eval(), since it can inject code
-            const result = Function("return " + expression)();
-
-            // Handle division by zero error, which is not possible
-            if (result === Infinity) {
-                throw new Error();
-            }
-
-            // Display result
-            display.value = result;
-            // Allow continuation of calculation
-            expression = String(result);
-        }
-        catch (e) {
+        // Handle division by zero error, which is not possible
+        if (result === Infinity) {
+            console.log("1");
             display.value = "Syntax Error";
             expression = "";
         }
+
+        // Display result
+        display.value = result;
+        // Allow continuation of calculation
+        expression = String(result);   
     }
     else if (key === "C") // Clears the display
     {
@@ -42,9 +38,10 @@ calculator.addEventListener('click', event => {
     else 
     {
         // If the expression starts with not a number, then it is an error. If the input is two consecutive NaN, then it is an error.
-        // integer signs is not a feature in this simple calculator
+        // integer signs for calculation is not a feature in this simple calculator, but results can still be negative.
         if ((isNaN(Number(key)) && !expression) || (isNaN(Number(key)) && isNaN(Number(expression.slice(-1))))) 
         {
+            console.log("2");
             display.value = "Syntax Error";
         }
         else // If the key is a valid input, then store and display.
@@ -52,6 +49,7 @@ calculator.addEventListener('click', event => {
             if (display.value === "Syntax Error")
             {
                 display.value = "";
+                expression = "";
             }
             expression += key;
             display.value = expression;
